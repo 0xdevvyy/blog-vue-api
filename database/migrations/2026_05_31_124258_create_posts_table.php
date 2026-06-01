0b429ac1a8faad62bc25a646a181cebd7f774fd1
0xdevvyy <pullhead45@gmail.com>
2026-06-01 10:15:47 +0800

    chore(factory-seeders): creating fake data

--- database/migrations/2026_05_31_124258_create_posts_table.php
+++ database/migrations/2026_05_31_124258_create_posts_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete(); 
             $table->string('title')->nullable();
-            // $table->string('tag');
             $table->string('slug')->unique()->nullable();
             $table->longText('content')->nullable();
             $table->string('description')->nullable();
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,16 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       $tagIds = Tag::pluck('id');
+
+        Post::factory()
+            ->count(10)
+            ->create()
+            ->each(function ($post) use ($tagIds) {
+                $post->tags()->attach(
+                    $tagIds->random(rand(1, 3))
+                );
+    });
+
     }
 }
